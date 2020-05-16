7d2f67467cb0a88ae60e7ee7cb9e6dc0068ba265
krishna Gupta <krishgupta43@gmail.com>
2020-05-16 15:29:09 +0530

    Updates

---
+++ precache-manifest.c8cdc24b53522f05e892ed7cd7e52f87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615a391731df06b8c46ae080d800bfe3",
+    "url": "/hello_world/index.html"
+  },
+  {
+    "revision": "d9e96e4595d8b3e8d19d",
+    "url": "/hello_world/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1611a8c2d2c2be80ab3c",
+    "url": "/hello_world/static/js/2.5b7492d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello_world/static/js/2.5b7492d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e96e4595d8b3e8d19d",
+    "url": "/hello_world/static/js/main.7a6f8d56.chunk.js"
+  },
+  {
+    "revision": "49f911dfd945e6dc92c6",
+    "url": "/hello_world/static/js/runtime-main.d0eba83b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello_world/static/media/logo.5d5d9eef.svg"
+  }
+]);
